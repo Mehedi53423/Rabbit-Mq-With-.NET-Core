77f50e221330d0619e1cda246ccd13252077741b
Md. Mehedi Hasan <mehedi53423@gmail.com>
2023-08-26 01:18:55 +0600

    BookingsController Added

--- FormulaAirLine.API/Controllers/BookingsController.cs
+++ FormulaAirLine.API/Controllers/BookingsController.cs
@@ -1,15 +1,17 @@
-﻿using FormulaAirLine.API.Services.Interfaces;
-using Microsoft.AspNetCore.Http;
+﻿using FormulaAirLine.API.Models;
+using FormulaAirLine.API.Services.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FormulaAirLine.API.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/[controller]/[action]")]
     [ApiController]
     public class BookingsController : ControllerBase
     {
         private readonly ILogger<BookingsController> _logger;
         private readonly IMessageProducer _messageProducer;
+
+        public static readonly List<Booking> _bookings = new();
 
         public BookingsController(
             ILogger<BookingsController> logger,
@@ -18,6 +20,17 @@ namespace FormulaAirLine.API.Controllers
         {
             _logger = logger;
             _messageProducer = messageProducer;
+        }
+
+        [HttpPost]
+        public IActionResult CreatingBooking(Booking newBooking)
+        {
+            if(!ModelState.IsValid) return BadRequest();
+
+            _bookings.Add(newBooking);
+            _messageProducer.SendingMessage(newBooking);
+
+            return Ok();
         }
     }
 }
