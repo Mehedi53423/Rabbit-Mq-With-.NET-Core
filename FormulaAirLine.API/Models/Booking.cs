41fd2f353b20d11d5a3b28af9cac20d04964727f
Md. Mehedi Hasan <mehedi53423@gmail.com>
2023-08-25 23:21:55 +0600

    Initial Commit

---
+++ FormulaAirLine.API/Models/Booking.cs
@@ -0,0 +1,12 @@
+﻿namespace FormulaAirLine.API.Models
+{
+    public class Booking
+    {
+        public int Id { get; set; }
+        public string PassengerName { get; set; } = string.Empty;
+        public string PassportNumber { get; set; } = string.Empty;
+        public string From { get; set; } = string.Empty;
+        public string To { get; set; } = string.Empty;
+        public int Status { get; set; }
+    }
+}
